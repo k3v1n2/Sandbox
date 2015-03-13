deeed1119a356404eec366c8bb8cf23f92b8e4d2
Kevin Walters <kevin.walters@manheim.com>
2015-03-13 13:38:42 -0400

    Using lambdas & Functional Interfaces

--- java/Lambdas/src/com/example/BasicExamples.java
+++ java/Lambdas/src/com/example/BasicExamples.java
@@ -2,11 +2,20 @@ package com.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by kjw on 3/13/15.
  */
 public class BasicExamples {
+
+    public static void printColor(String color, Printer printer) {
+        printer.print(color);
+    }
+
+    public static void printColorStandardInterface(String color, Consumer<String> consumer) {
+        consumer.accept(color);
+    }
 
     public static void main(String[] args) {
 
@@ -24,5 +33,17 @@ public class BasicExamples {
         System.out.println("---------");
 
         colorList.forEach(System.out::println);
+
+        System.out.println("---------");
+
+        BasicExamples.printColor(colors[0], (color) -> System.out.println(color));
+
+        System.out.println("---------");
+
+        colorList.forEach((selectedColor) -> BasicExamples.printColor(selectedColor, (color) -> System.out.println(color)));
+
+        System.out.println("---------");
+
+        colorList.forEach((selectedColor) -> BasicExamples.printColorStandardInterface(selectedColor, (color) -> System.out.println(color)));
     }
 }
---
+++ java/Lambdas/src/com/example/Printer.java
@@ -0,0 +1,8 @@
+package com.example;
+
+/**
+ * Created by kjw on 3/13/15.
+ */
+public interface Printer {
+    public void print(String data);
+}
